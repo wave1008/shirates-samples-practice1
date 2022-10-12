c4fe3c81edf2148d5c76ee95a3ab4f1fa4844a31
wave1008 <wave1008@gmail.com>
2022-10-13 00:30:56 +0900

    Practice1_ja

---
+++ Practice1_ja/src/test/kotlin/CalculatorTest1.kt
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test
+import shirates.core.driver.commandextension.select
+import shirates.core.driver.commandextension.tap
+import shirates.core.driver.commandextension.tapAppIcon
+import shirates.core.driver.commandextension.textIs
+import shirates.core.testcode.UITest
+
+class CalculatorTest1: UITest() {
+
+    @Test
+    fun test1() {
+
+        scenario {
+            case(1){
+                condition {
+                    it.tapAppIcon("電卓")
+                }.action {
+                    it.tap("1")
+                        .tap("2")
+                        .tap("3")
+                        .tap("+")
+                        .tap("4")
+                        .tap("5")
+                        .tap("6")
+                        .tap("=")
+                }.expectation {
+                    it.select("#result_final")
+                        .textIs("579")
+                }
+            }
+        }
+    }
+}
